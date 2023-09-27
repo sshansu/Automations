fcf1a059c2cd121fc25e5fd5cc75343e68aee988
sshansu <104137909+sshansu@users.noreply.github.com>
2023-09-27 11:40:38 +0530

    Add files via upload

---
+++ CompliancePolicy_JSON_Logic.ps1
@@ -0,0 +1,13 @@
+﻿$App = "Google Chrome"
+$installedApp = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | `
+Select-Object DisplayName, DisplayVersion, Publisher | where{$_.DisplayName -eq $App}
+If($installedApp)
+    {
+        $hash = @{ AppName = $installedApp.DisplayName; Version = $installedApp.DisplayVersion; Publisher = $installedApp.Publisher; AppPresent = $true}
+        return $hash | ConvertTo-Json -Compress
+    }
+else
+    {
+        $hash = @{ AppName = $App; Version = $null; Publisher = $null; AppPresent = $false}
+        return $hash | ConvertTo-Json -Compress
+    }
