60e8396330e54474e3359ebea242ab2077dad33f
sshansu <104137909+sshansu@users.noreply.github.com>
2023-09-28 20:04:14 +0530

    Update Create_Virtual_Machine.ps1

--- Create_Virtual_Machine.ps1
+++ Create_Virtual_Machine.ps1
@@ -1,5 +1,14 @@
-﻿   
-#$Admincred = Get-Credential -UserName admin@MngEnvMCAP722212.onmicrosoft.com -Message "Enter Password"
+<#
+This sample script is not supported under any Microsoft standard support program or service.
+The sample script is provided AS IS without warranty of any kind.
+Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
+The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
+In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for 
+loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if 
+Microsoft has been advised of the possibility of such damages
+#>
+
+#$Admincred = Get-Credential -UserName "GlobalAdminAccount" -Message "Enter Password"
 $subcriptionID =""
 $resourceGroup = ""
 $location = "eastus2"
@@ -40,13 +49,6 @@ Write-host "$subscriptionID"
 
 write-host "Accepting local admin password for the VM" -ForegroundColor DarkYellow
 $cred = Get-Credential -UserName $LocalAdmin -Message "Enter new local admin password"
-
-#$subscriptionContext = Select-AzSubscription -Subscription $subcriptionID
-
-# Setting Azure Context 
-#Set-AzContext -Subscription "ME-MngEnvMCAP722212-sshansu-2"
-
-#Get-AzResourceGroup -Name SpaceNet | remove-azresourcegroup -Force
 
 foreach($vmName in $vmNames)
     {
