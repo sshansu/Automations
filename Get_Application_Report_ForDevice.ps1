4a579fe681eaf0cf67d21a40ef6814ebb4142de2
sshansu <104137909+sshansu@users.noreply.github.com>
2023-09-28 20:26:33 +0530

    Update Get_Application_Report_ForDevice.ps1

--- Get_Application_Report_ForDevice.ps1
+++ Get_Application_Report_ForDevice.ps1
@@ -1,9 +1,22 @@
-﻿<#
+<#
 This sample script is not supported under any Microsoft standard support program or service.
 The sample script is provided AS IS without warranty of any kind.
 Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
 The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
-In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages
+In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for 
+loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if 
+Microsoft has been advised of the possibility of such damages
+
+This script requires azure application registered with below permissions granted to graph API
+
+Device.Read.All
+Device.Read
+DeviceManagementApps.Read.All
+DeviceManagementConfiguration.Read.All
+DeviceManagementServiceConfig.Read.All
+Directory.Read.All
+
+For the script to work, you must have exported the imported GPO settings in a CSV Format. We'll have to execute Export_GPOSettings_from_GPOAnalytics.ps1 first. 
 #>
 
 #$global:authToken = $null
