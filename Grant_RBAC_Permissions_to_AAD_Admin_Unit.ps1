64ffb528bd3bd3857351e881379d1bae85b5a70a
sshansu <104137909+sshansu@users.noreply.github.com>
2023-09-28 21:03:05 +0530

    Update Grant_RBAC_Permissions_to_AAD_Admin_Unit.ps1

--- Grant_RBAC_Permissions_to_AAD_Admin_Unit.ps1
+++ Grant_RBAC_Permissions_to_AAD_Admin_Unit.ps1
@@ -5,8 +5,18 @@ Microsoft further disclaims all implied warranties including, without limitation
 The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
 In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages
 
-This script is to automate RBAC Role assignment of HelpDesk Administrator and User Administrator to Azure AD Administrative Unit.
+This script requires azure application registered with below permissions granted to graph API
 
+AdministrativeUnit.ReadWrite.All
+DeviceManagementRBAC.ReadWrite.All
+Directory.Read.All
+Group.Read.All
+RoleManagement.Read.All
+RoleManagement.ReadWrite.Directory
+User.Read
+
+This script is to automate RBAC Role assignment of HelpDesk Administrator and User Administrator to Azure AD Administrative Unit.
+Creates Scope Tags in Intune and assigns it to the Group. 
 #>
 
 Param (
