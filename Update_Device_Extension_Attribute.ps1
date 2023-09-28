388338ceda0ea633d1f0c68d544dc60aed16b78b
sshansu <104137909+sshansu@users.noreply.github.com>
2023-09-28 22:04:26 +0530

    Update Update_Device_Extension_Attribute.ps1

--- Update_Device_Extension_Attribute.ps1
+++ Update_Device_Extension_Attribute.ps1
@@ -1,9 +1,25 @@
-﻿<#
+<#
 This sample script is not supported under any Microsoft standard support program or service.
 The sample script is provided AS IS without warranty of any kind.
 Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
 The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
-In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages
+In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for 
+loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if 
+Microsoft has been advised of the possibility of such damages
+
+This script requires azure application registered with below permissions granted to graph API
+
+Device.ReadWrite.All
+DeviceManagementManagedDevices.ReadWrite.All
+Directory.Read.All
+Group.Read.All
+GroupMember.Read.All
+User.Read
+
+The script updates extension attribute to a particular region for all the iOS and Android for Work devices owned by users of a specific group. 
+Specify the User Group Name and Device Location on lines 106 and 107
+Once the script executes, create a dynamic device group using ExtensionAttribute criteria. 
+
 #>
 
 #Initialize Variables
