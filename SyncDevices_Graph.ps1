c5c5db2915ba1b8fe1444feeff6a1d846d44b5a7
sshansu <104137909+sshansu@users.noreply.github.com>
2023-09-28 21:42:14 +0530

    Update SyncDevices_Graph.ps1

--- SyncDevices_Graph.ps1
+++ SyncDevices_Graph.ps1
@@ -1,11 +1,20 @@
-﻿<#
+<#
 This sample script is not supported under any Microsoft standard support program or service.
 The sample script is provided AS IS without warranty of any kind.
 Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
 The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
-In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, 
-damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, 
-even if Microsoft has been advised of the possibility of such damages
+In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for 
+loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if 
+Microsoft has been advised of the possibility of such damages
+
+This script requires azure application registered with below permissions granted to graph API
+
+DeviceManagementManagedDevices.PrivilegedOperations.All
+DeviceManagementManagedDevices.Read.All
+Directory.Read.All
+User.Read
+
+The script invokes a remote wipe on intune enrolled devices. 
 #>
 
 #Initialize Variables
