4ab45ba627b69e83e9be1d2646098e7c35ffbf87
sshansu <104137909+sshansu@users.noreply.github.com>
2023-09-28 21:44:49 +0530

    Update Update_DeviceOwnership_to_Corporate.ps1

--- Update_DeviceOwnership_to_Corporate.ps1
+++ Update_DeviceOwnership_to_Corporate.ps1
@@ -3,7 +3,11 @@ This sample script is not supported under any Microsoft standard support program
 The sample script is provided AS IS without warranty of any kind.
 Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose.
 The entire risk arising out of the use or performance of the sample scripts and documentation remains with you.
-In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages
+In no event shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, 
+damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation,
+even if Microsoft has been advised of the possibility of such damages.
+
+This script updates the ownership type of personal devices of a specific Azure AD group to "Corporate"/ 
 #>
 
 #Initialize Variables
